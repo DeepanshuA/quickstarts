0620b7fff23d1a15666bcc88e3df140632c49d19
deepanshuagarwal <deepanshu.agarwal1984@gmail.com>
2022-09-13 16:00:32 +0530

    Add bulk subscribe stuff
    Signed-off-by: deepanshuagarwal <deepanshu.agarwal1984@gmail.com>

--- pub_sub/javascript/http/order-processor/index.js
+++ pub_sub/javascript/http/order-processor/index.js
@@ -11,15 +11,53 @@ app.get('/dapr/subscribe', (_req, res) => {
         {
             pubsubname: "orderpubsub",
             topic: "orders",
-            route: "orders"
-        }
+            route: "orders",
+            metadata: { 
+                // rawPayload: "true",
+                // maxBulkCount: "4", 
+                // maxBulkLatencyMilliSeconds: "100000",
+            //     // maxBatchSizeInBytes: "",
+                // bulkSubscribe: "true",
+            }, 
+            // routes:{
+            //     // "default":"event/pubsub/TestTIntegrationEvent/v1",
+            //     "rules":[
+            //         {
+            //             // match:'event.type == "com.dapr.event.sent"',
+            //             match: 'event == "hellozzzzzz worldieeeeee"',
+            //             path:"orders"
+            //         }
+            //     ]
+            // },
+            deadLetterTopic: "TestdeadLetterTEvent"
+    
+        },
+        // {
+        //     pubsubname: "orderpubsub",
+        //     topic: "ordersAgain",
+        //     route: "ordersAgain",
+        //     metadata: { 
+        //         rawPayload: "true",
+        //         maxBatchSize: "10", 
+        //         bulkSubscribe: "true",
+        //     } 
+          
+        // }
     ]);
 });
 
 // Dapr subscription routes orders topic to this route
 app.post('/orders', (req, res) => {
-    console.log("Subscriber received:", req.body.data);
-    res.sendStatus(200);
+    console.log("============================New batch received==================Size: ", req.body.data.length);
+    let output = [];
+    let tmp;
+    for(let event in req.body.data) {        
+        console.log("Subscriber received:", req.body.data[event].event);
+        tmp = {"status" : "SUCCESS", "entryID": req.body.data[event].entryID};
+        output.push(tmp);
+    }
+    res.status(200).json({"statuses": output})
 });
 
 app.listen(APP_PORT);
+  
