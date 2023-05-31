21d1777a92217ff4a28baf016dbf068156ea22a3
Deepanshu Agarwal <deepanshu.agarwal1984@gmail.com>
2023-06-01 01:58:32 +0530

    kafka with eh and sasl - Receiver
    Signed-off-by: Deepanshu Agarwal <deepanshu.agarwal1984@gmail.com>

--- bindings/javascript/sdk/batch/index.js
+++ bindings/javascript/sdk/batch/index.js
@@ -11,48 +11,37 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-/*
-dapr run --app-id batch-sdk --app-port 5002 --dapr-http-port 3500 --resources-path ../../../components -- node index.js
-*/
+import { DaprClient, DaprServer, CommunicationProtocolEnum } from "@dapr/dapr";
 
-import { DaprClient, DaprServer } from "@dapr/dapr";
-import fs from 'fs';
-
-const cronBindingName = "cron";
-const postgresBindingName = "sqldb";
+const kafkaBindingName = "kafkabind";
 
 const daprHost = process.env.DAPR_HOST || 'http://localhost';
-const daprPort = process.env.DAPR_HTTP_PORT || '3500';
+const daprPort =  '3500';
 const serverHost = "127.0.0.1";
-const serverPort = process.env.APP_PORT || '5005';
+const serverPort = '3000';
 
 const client = new DaprClient(daprHost, daprPort);
 
 const server = new DaprServer(serverHost, serverPort, daprHost, daprPort);
-
-async function start() {
-    await server.binding.receive(cronBindingName,processBatch);
-    await server.start();
-}
 
 start().catch((e) => {
     console.error(e);
     process.exit(1);
 });
 
-
-async function processBatch(){
-    const loc = '../../../orders.json';
-    fs.readFile(loc, 'utf8', (err, data) => {
-        const orders = JSON.parse(data).orders;
-        orders.forEach(order => {
-            let sqlCmd = `insert into orders (orderid, customer, price) values (${order.orderid}, '${order.customer}', ${order.price});`;
-            let payload = `{"sql": "${sqlCmd}"} `;
-            console.log(sqlCmd);
-            client.binding.send(postgresBindingName, "exec", "", JSON.parse(payload));
-        });
-        console.log('Finished processing batch');
+async function start() {
+    console.log("server port: ", serverPort)
+    console.log("dapr port: ", daprPort)
+    const server = new DaprServer({
+        serverHost,
+        serverPort,
+        communicationProtocol: CommunicationProtocolEnum.HTTP,
+        clientOptions: {
+            daprHost,
+            daprPort, 
+        }
     });
-    return 0;
-}
+    await server.binding.receive(kafkaBindingName, async (orderId) => console.log(`Received Message: ${JSON.stringify(orderId)}`));
+    await server.start();
 
+}
